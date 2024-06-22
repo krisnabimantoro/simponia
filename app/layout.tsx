94d6101d1229d7570d662149008874e621845599
krisnabimantoro <krisnabmntr@gmail.com>
2024-06-22 11:45:22 +0700

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -16,6 +18,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang="en" className="dark scroll-smooth">
       <body className={montserrat.className}>{children}</body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  });
   return (
     <div>
       <Navbar />
@@ -13,7 +20,7 @@ export default function Home() {
             {/* <div className="absolute bottom-0 -left-20 w-36 h-48 md:w-72 md:h-72 lg:w-72 lg:h-72 bg-secondary rounded-full blur-3xl bl opacity-30 animate-blob animation-delay-2000"></div>
 
             <div className="absolute top-0 lg:right-0 w-24 h-24 md:w-80 md:h-80 lg:w-80 lg:h-80 right-10 bg-secondary rounded-full blur-3xl opacity-20 animate-blob animation-delay-4000"></div> */}
-            <div className="self-center mx-auto relative">
+            <div className="self-center mx-auto ">
               <div className="flex flex-wrap justify-center mb-8 gap-4">
                 <Image src="/statis/rpl.png" width={50} height={20} className="mr-4" alt="/" />
                 <Image src="/statis/game.png" width={50} height={20} className="mr-4" alt="/" />
@@ -35,9 +42,9 @@ export default function Home() {
       <section id="step" className="bg-white">
         <div className="container">
           <div className="flex flex-wrap h-96">
-            <div className="mt-24 mx-auto relative">
+            <div className="my-24 mx-auto relative">
               <h1 className="text-primary font-bold text-2xl text-center">AYO MULAI PORTFOLIO KAMU</h1>
-              <div className="flex justify-center items-center space-x-64 mt-12">
+              <div className="flex justify-center items-center space-x-64 mt-24 " data-aos="fade-up">
                 <div className="text-center flex flex-col items-center">
                   <Image src="/statis/portfolio/Enter.svg" width={80} height={20} className=" mb-2" alt="/" />
                   <h5 className="text-primary font-bold text-xl">LOGIN</h5>
@@ -68,9 +75,9 @@ export default function Home() {
       </section>
 
       <section id="" className="bg-white">
-        <div className="container py-28">
-          <div className="flex flex-wrap w-full">
-            <div className="w-1/2 self-center pl-20">
+        <div className="container py-48">
+          <div className="flex flex-wrap w-full" data-aos="fade-left">
+            <div className="w-1/2 self-center pl-20 " >
               <h1 className="text-primary font-bold text-3xl">Apa itu Portfolio?</h1>
               <h2 className="text-justify mt-4 text-black">
                 Lorem ipsum dolor sit amet consectetur. Quisque purus risus in purus at a. Tincidunt et sapien donec id integer pulvinar. Eu
@@ -91,7 +98,7 @@ export default function Home() {
       <section className="bg-white">
         <div className="container ">
           <div className="flex bg-gradient-to-tr from-topgrad to-secondary rounded-3xl drop-shadow-xl">
-            <div className="self-center mx-auto">
+            <div className="self-center mx-auto" data-aos="fade-up">
               <h1 className="text-white text-center mt-20 text-3xl font-bold">Kategori Portfolio</h1>
               <div className="grid grid-cols-4 grid-rows-1 gap-12 pt-12 mb-24 ">
                 <div className="border border-white w-56 h-72 rounded-lg p-5">
@@ -155,12 +162,12 @@ export default function Home() {
       </section>
 
       <section id="" className="bg-white">
-        <div className="container py-28">
-          <div className="flex flex-wrap w-full">
+        <div className="container py-48">
+          <div className="flex flex-wrap w-full" data-aos="fade-up">
             <div className="w-1/2 flex ">
               <Image src="/statis/hero.png" width={380} height={20} className="mx-auto rounded-3xl" alt="/" />
             </div>
-            <div className="w-1/2 self-center pl-20">
+            <div className="w-1/2 self-center pl-20" >
               <h1 className="text-primary text-right font-bold text-3xl">About Simponia</h1>
               <h2 className="text-justify mt-4 text-black">
                 Lorem ipsum dolor sit amet consectetur. Quisque purus risus in purus at a. Tincidunt et sapien donec id integer pulvinar. Eu
